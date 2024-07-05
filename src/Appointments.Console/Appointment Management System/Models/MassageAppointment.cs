cfbd2d24052bfcaabdc9470530ef8ac82635f3fb
Dimitris <dimitrisoiko1@gmail.com>
2024-07-05 15:57:37 +0300

    - Moved inherited classes to new files

---
+++ src/Appointments.Console/Appointment Management System/Models/MassageAppointment.cs
@@ -0,0 +1,19 @@
+﻿namespace Appointment_Management_System.Models
+{
+    public class MassageAppointment : Appointment
+    {
+        public MassageServices MassageServices { get; set; }
+        public MasseusePreference Preference { get; set; }
+        public MassageAppointment(Customer customer, ServiceType serviceType, DateTime date, string time, string notes,
+            MassageServices massageServices, MasseusePreference preference) : base(customer, serviceType, date, time, notes)
+        {
+            MassageServices = massageServices;
+            Preference = preference;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Service Type: {ServiceType}, Masseuse Preference: {Preference}";
+        }
+    }
+}
